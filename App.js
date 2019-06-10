d670e205c97785d11a832f2730348da51da61e1c
Tetsukick <endeavor.0329@gmail.com>
2019-06-10 20:37:29 +0900

    メモリストの追加、AppBarのスタイル適用

--- App.js
+++ App.js
@@ -1,13 +1,37 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import BodyText from './src/elements/BodyText';
-
 export default class App extends React.Component {
   render () {
     return (
       <View style={styles.container}>
-        <BodyText>TEST</BodyText>
+        <View style={styles.appbar}>
+          <View>
+            <Text style={styles.appbarTitle}>MEMOT</Text>
+          </View>
+        </View>
+
+        <View style={styles.memoList}>
+          <View style={styles.memoListItem}>
+            <Text style={styles.memoTitle}>メモタイトル</Text>
+            <Text style={styles.memoDate}>2019/06/10</Text>
+          </View>
+
+          <View style={styles.memoListItem}>
+            <Text style={styles.memoTitle}>メモタイトル</Text>
+            <Text style={styles.memoDate}>2019/06/10</Text>
+          </View>
+
+          <View style={styles.memoListItem}>
+            <Text style={styles.memoTitle}>メモタイトル</Text>
+            <Text style={styles.memoDate}>2019/06/10</Text>
+          </View>
+          
+          <View style={styles.memoListItem}>
+            <Text style={styles.memoTitle}>メモタイトル</Text>
+            <Text style={styles.memoDate}>2019/06/10</Text>
+          </View>
+        </View>
       </View>
     );
   }
@@ -20,4 +44,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  appbar: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    height: 78,
+    paddingTop: 30,
+    backgroundColor: '#265366',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  appbarTitle: {
+    color: '#fff',
+    fontSize: 18
+  }
 });
