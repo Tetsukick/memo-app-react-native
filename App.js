69e38b93b9776e7168aedc4c7989375d6cc0aec0
Tetsukick <endeavor.0329@gmail.com>
2019-06-11 10:44:42 +0900

    memoテーブル、ADDボタンのスタイルの追加

--- App.js
+++ App.js
@@ -32,6 +32,10 @@ export default class App extends React.Component {
             <Text style={styles.memoDate}>2019/06/10</Text>
           </View>
         </View>
+
+        <View style={styles.memoAddButton}>
+          <Text style={styles.memoAddButtonTitle}>+</Text>
+        </View>
       </View>
     );
   }
@@ -40,9 +44,10 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fffdf6',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 78,
   },
   appbar: {
     position: 'absolute',
@@ -53,10 +58,49 @@ const styles = StyleSheet.create({
     paddingTop: 30,
     backgroundColor: '#265366',
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.5,
+    shadowRadius: 3,
+    zIndex: 10,
   },
   appbarTitle: {
     color: '#fff',
     fontSize: 18
+  },
+  memoList: {
+    width: '100%',
+    flex: 1,
+  },
+  memoListItem: {
+    backgroundColor: '#fff',
+    padding: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ddd'
+  },
+  memoTitle: {
+    fontSize: 18,
+    marginBottom: 4
+  },
+  memoAddButton: {
+    position: 'absolute',
+    bottom: 32,
+    right: 32,
+    width: 48,
+    height: 48,
+    backgroundColor: '#E31676',
+    borderRadius: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 3,
+  },
+  memoAddButtonTitle: {
+    fontSize: 32,
+    color: '#fff',
+    lineHeight: 32,
   }
 });
