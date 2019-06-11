80df2d07b69772e2ed73ce172ea1048241363f3f
Tetsukick <endeavor.0329@gmail.com>
2019-06-11 12:31:21 +0900

    画面のページ単位でのコンポーネント化

--- App.js
+++ App.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import MemoList from './src/components/MemoList';
 import AppBar from './src/components/AppBar';
-import CircleButton from './src/elements/CircleButton';
+import MemoListScreen from './src/screens/MemoListScreen';
 
 export default class App extends React.Component {
   render () {
     return (
       <View style={styles.container}>
         <AppBar />
-
-        <MemoList />
-
-        <CircleButton>+</CircleButton>
+        <MemoListScreen />
       </View>
     );
   }
